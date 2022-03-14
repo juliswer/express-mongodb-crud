f2411936ffe46815349104814d51a0039c043160
Julian Swerdlin <julianswer36@gmail.com>
2022-03-14 15:46:41 -0300

    uploading routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 import express from "express";
+import { create } from "express-handlebars";
 import indexRoutes from "./routes/index.routes";
+import path from "path";
 
 const app = express();
 
+// View Engine
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  create({
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    defaulLayout: "main",
+    extname: ".hbs",
+  }).engine
+);
+app.set("view engine", ".hbs");
+
+// Routes
 app.use(indexRoutes);
 
-export default app
+export default app;
