f70e89150690c1341276051fdd11bb122fe29dda
Julian Swerdlin <julianswer36@gmail.com>
2022-03-14 17:03:46 -0300

    routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,32 +4,36 @@ import Task from "../models/Task";
 const router = Router();
 
 router.get("/", async (req, res) => {
-  
-  const data = await Task.find()
-
+  const data = await Task.find();
   res.render("index");
 });
 
 router.post("/tasks/add", async (req, res) => {
   const task = Task(req.body);
-  const savedTask = await task.save()
-  console.log(savedTask)
-  res.json({
-    message: 'Great! Task added successfully. Data: ',
-    savedTask
-  })
+  await task.save();
+  res.redirect("/");
 });
 
 router.get("/about", (req, res) => {
   res.render("about");
 });
 
+router.get("/:id/task/delete", async (req, res) => {
+  try {
+    const { id } = req.params;
+    Task.findByIdAndDelete(id);
+
+    res.json({ message: "Task deleted successfully" });
+  } catch (error) {
+    console.log("Error: " + error.message);
+  }
+});
+
 router.get("/:id/edit", async (req, res) => {
-  
-  const {id} = req.params
-  const data = await Task.findById(id)
-  console.log(data)
-  
+  const { id } = req.params;
+  const data = await Task.findById(id);
+  console.log(data);
+
   res.render("edit");
 });
 
