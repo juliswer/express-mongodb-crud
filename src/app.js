cd78c3a4a7fbe7d60874ae7fff7e1172e2d41d7a
Julian Swerdlin <julianswer36@gmail.com>
2022-03-14 16:44:46 -0300

    routes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,12 @@ import express from "express";
 import { create } from "express-handlebars";
 import indexRoutes from "./routes/index.routes";
 import path from "path";
+import morgan from "morgan";
+
 const app = express();
+
+// Middlewares
+app.use(morgan("dev"));
 
 // View Engine
 app.set("views", path.join(__dirname, "views"));
--- src/models/Task.js
+++ src/models/Task.js
@@ -1,11 +1,28 @@
 import { Schema, model } from "mongoose";
 
-const TaskSchema = new Schema({
-  title: String,
-  description: String,
-  done: Boolean,
-}, {
+const TaskSchema = new Schema(
+  {
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 5
+    },
+    description: {
+        type: String,
+        trim: true,
+        minlength: 10
+    },
+    done: {
+        type: Boolean,
+        default: false
+    },
+  },
+  {
     timestamps: true,
-});
+    versionKey: false
+  }
+);
 
 export default model("Task", TaskSchema);
